256e041057b60ca0ad349c14693129815ef77544
Nandini3103 <61120008+Nandini3103@users.noreply.github.com>
2020-03-19 16:23:13 +0530

    Initial Commit

---
+++ dice app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice app
+//
+//  Created by Nandini Aggarwal on 19/03/20.
+//  Copyright © 2020 Nandini Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
